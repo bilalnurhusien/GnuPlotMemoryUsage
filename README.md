40de1ed3bfa9c284d4084c568c261b184f0498ea
Bilal Nurhusien <bilal.nurhusien@t2systems.com>
2018-03-26 19:53:21 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 This tool will ssh into the remote host specified by the IP address argument.
 It will monitor the memory usage on the remote host and display a graph.
+To run:
+
+./memorymon.sh XXX.XXX.XXX.XXX
--- memorymon.sh
+++ memorymon.sh
@@ -11,6 +11,7 @@ export ip_addr=${1}
 export free_mon_file=freemon.sh
 export tmp_free_mon_path=/tmp/${free_mon_file}
 export tmp_memory_data_path=/tmp/memoryusage.dat
+export memory_usage_plot=memoryusageplot.in
 
 #copy script to remote host
 scp ${free_mon_file} "root@${ip_addr}:/tmp/"
@@ -27,10 +28,10 @@ while (true) do
 	scp "root@${ip_addr}:${tmp_memory_data_path}" ${tmp_memory_data_path}
 
 	#run gnuplot
-	gnuplot -e "file_name='${tmp_memory_data_path}'" plot.in
+	gnuplot -e "file_name='${tmp_memory_data_path}'" $memory_usage_plot
 
 	#exit gnuplot window
-	pkill gnuplot
+	pkill "gnuplot $memory_usage_plot"
 
 done
 
