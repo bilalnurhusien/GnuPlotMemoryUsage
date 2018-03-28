21e056b7eb42aa7ce4f2e6b5ab8ae300fbe7302a
Bilal Nurhusien <bilal.nurhusien@t2systems.com>
2018-03-27 17:32:05 -0700

    Fix typo

--- freemon.sh
+++ freemon.sh
@@ -8,8 +8,8 @@ while true; do
 	file_size=`cat /tmp/memoryusage.dat | wc -l`
 	if [ $file_size -gt $max_file_length ]
 		then
-			sed -e '1,${trim_length}d' < /tmp/memoryusage.dat > /tmp/memoryusage.dat
-			mv /tmp/memoryusage.dat /tmp/memoryusage.dat
+			sed -e '1,${trim_length}d' < /tmp/memoryusage.dat > /tmp/tmp_memoryusage.dat
+			mv /tmp/tmp_memoryusage.dat /tmp/memoryusage.dat
 	fi
 
 	free -k | awk -v timestamp=$(date +"%H:%M") 'NR==2{print timestamp,$2,$3,$4}' >> /tmp/memoryusage.dat; sleep 60;
